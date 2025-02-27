6eae8c9d5a4f66ff891faf1f257d668987c9c40b
mmintpk <143266308+mmintpk@users.noreply.github.com>
2025-02-27 19:12:41 +0700

    Create DealerInstalledOption.java

---
+++ Patchadaporn/HolidayTravelVehicles/DealerInstalledOption.java
@@ -0,0 +1,15 @@
+class DealerInstalledOption {
+  private String optionCode;
+  private String description;
+  private double price;
+  private int year;
+
+  public DealerInstalledOption(String optionCode, String description, double price, int year) {
+      this.optionCode = optionCode;
+      this.description = description;
+      this.price = price;
+      this.year = year;
+  }
+
+  // Getters and Setters
+}
