acdbacafb852b4b9815eec6243e869431123a97f
mmintpk <143266308+mmintpk@users.noreply.github.com>
2025-02-27 19:13:10 +0700

    Create SalesInvoice.java

---
+++ Patchadaporn/HolidayTravelVehicles/SalesInvoice.java
@@ -0,0 +1,15 @@
+class SalesInvoice {
+  private String invoiceNumber;
+  private String date;
+  private double price;
+  private double tax;
+
+  public SalesInvoice(String invoiceNumber, String date, double price, double tax) {
+      this.invoiceNumber = invoiceNumber;
+      this.date = date;
+      this.price = price;
+      this.tax = tax;
+  }
+
+  // Getters and Setters
+}
