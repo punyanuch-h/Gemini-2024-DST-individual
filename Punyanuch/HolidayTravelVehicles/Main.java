6d00aec5ff86a16b37f2e4d5aea2942963e4840f
punyanuch-h <108443864+punyanuch-h@users.noreply.github.com>
2025-02-27 22:12:30 +0700

    Update Main.java

--- Punyanuch/HolidayTravelVehicles/Main.java
+++ Punyanuch/HolidayTravelVehicles/Main.java
@@ -1,5 +1,5 @@
 import java.util.List;
-
+// Test minicase4
 public class Main {
     public static void main(String[] args) {
         Vehicle test = new Vehicle("001", "Toyota Camry", "model_test", 1989, "Toyota", 200000.00);
