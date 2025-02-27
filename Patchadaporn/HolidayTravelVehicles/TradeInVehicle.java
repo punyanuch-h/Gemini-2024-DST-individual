9fc665bc8a2fcec76e6b2e3136ac0bd9bdf79208
mmintpk <143266308+mmintpk@users.noreply.github.com>
2025-02-27 19:13:53 +0700

    Create TradeInVehicle.java

---
+++ Patchadaporn/HolidayTravelVehicles/TradeInVehicle.java
@@ -0,0 +1,15 @@
+class TradeInVehicle {
+  private String seminarNumber;
+  private String make;
+  private String model;
+  private int year;
+
+  public TradeInVehicle(String seminarNumber, String make, String model, int year) {
+      this.seminarNumber = seminarNumber;
+      this.make = make;
+      this.model = model;
+      this.year = year;
+  }
+
+  // Getters and Setters
+}
