a77be87b110ee2a668b560d8ff5d89ea1d311892
mmintpk <143266308+mmintpk@users.noreply.github.com>
2025-02-27 19:13:33 +0700

    Create Salesperson.java

---
+++ Patchadaporn/HolidayTravelVehicles/Salesperson.java
@@ -0,0 +1,11 @@
+class Salesperson {
+  private int id;
+  private String name;
+
+  public Salesperson(int id, String name) {
+      this.id = id;
+      this.name = name;
+  }
+
+  // Getters and Setters
+}
